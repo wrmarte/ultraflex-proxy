a8247fd6cb39c3252606b9c5d7286241600b5f9f
wrmarte <61038019+wrmarte@users.noreply.github.com>
2025-05-20 23:02:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const fetch = require('node-fetch');
 const fs = require('fs');
 const { Client: PgClient } = require('pg');
 
+
 // === TOKEN SETUP ===
 const TOKEN_NAME_TO_ADDRESS = {
   'ADRIAN': '0x7e99075ce287f1cf8cbcaaa6a1c7894e404fd7ea'
